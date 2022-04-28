b3ec2638df2083f8d872d1d534b0ff277ffedc4e
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2022-04-28 13:59:29 +0200

    Add some runs

--- code/PowerSupplyDataCollector.py
+++ code/PowerSupplyDataCollector.py
@@ -2,15 +2,23 @@ import serial
 import time
 import numpy as np
 from matplotlib import pyplot as plt
+from wakepy import keepawake # For keeping the computer turned awake when running
 
 # NOTE: This assumes that the power supply is connected to COM3. This can be changed in the code below.
 # NOTE: This assumes that the power supply has RS485 ID 01. This is set by holding down the "VSET" button.
 
-# Parameters:
-CC_CURRENT = 1 # CC current in A
+# Time units
+sec = 1
+min = 60 * sec
+h = 60 * min
+d = 24 * h
+
+# Parameters
+RUN_ID = "1"
+CC_CURRENT = 2 # CC current in A
 CC_VOLTAGE = 20 # CC max voltage in V
-MEASURING_TIME = 10 # Measuring time in s
-MEASURING_INTERVAL = 1 # Measuring interval in s
+MEASURING_TIME = 4 * h # Measuring time in s
+MEASURING_INTERVAL = 10 * sec # Measuring interval in s
 
 def byte_to_float(b):
     return float(b.decode())
@@ -77,50 +85,47 @@ def main():
     setSetCurrent(ser, CC_CURRENT)
     setSetVoltage(ser, CC_VOLTAGE)
     
-    # Turn on and start measuring
-    setOnState(ser, True)
-    time.sleep(1) # Sleep for 1 second before measurements start
-    t_array = np.array([])
-    I_array = np.array([])
-    U_array = np.array([])
-    start = time.time()
-    while time.time() - start < MEASURING_TIME:
-        t = time.time() - start
-        i = getOutputCurrent(ser)
-        u = getOutputVoltage(ser)
-        t_array = np.append(t_array, t)
-        I_array = np.append(I_array, i)
-        U_array = np.append(U_array, u)
-        # print(f"Data: u = {u}, i = {i}")
+    # Use keepawake context to prevent computer from going asleep when we are running
+    with keepawake(keep_screen_awake=False):
+        # Turn on and start measuring
+        setOnState(ser, True)
+        time.sleep(1) # Sleep for 1 second before measurements start
+        t_array = np.array([])
+        I_array = np.array([])
+        U_array = np.array([])
+        start = time.time()
+        while time.time() - start < MEASURING_TIME:
+            t = time.time() - start
+            i = getOutputCurrent(ser)
+            u = getOutputVoltage(ser)
+            t_array = np.append(t_array, t)
+            I_array = np.append(I_array, i)
+            U_array = np.append(U_array, u)
+            # print(f"Data: u = {u}, i = {i}")
 
-        # Save data
-        with open(f'data/PowerSupplyData_CURRENT-{CC_CURRENT}_VOLTAGE-{CC_VOLTAGE}_TIME-{MEASURING_TIME}_INTERVAL-{MEASURING_INTERVAL}.npy', 'wb') as f:
-            np.save(f, t_array)
-            np.save(f, I_array)
-            np.save(f, U_array)
+            # Save data
+            with open(f'data/PowerSupplyData_RUN_ID-{RUN_ID}_CURRENT-{CC_CURRENT}_VOLTAGE-{CC_VOLTAGE}_TIME-{MEASURING_TIME}_INTERVAL-{MEASURING_INTERVAL}.npy', 'wb') as f:
+                np.save(f, t_array)
+                np.save(f, I_array)
+                np.save(f, U_array)
+
+            time.sleep(MEASURING_INTERVAL)
 
         # Plot data
         plt.plot(t_array, I_array)
         plt.xlabel('Time (s)')
         plt.ylabel('Current (A)')
-        plt.savefig(f"data/plots/CURRENT_PowerSupplyData_CURRENT-{CC_CURRENT}_VOLTAGE-{CC_VOLTAGE}_TIME-{MEASURING_TIME}_INTERVAL-{MEASURING_INTERVAL}.png")
+        plt.savefig(f"data/plots/CURRENT_PowerSupplyData_RUN_ID-{RUN_ID}_CURRENT-{CC_CURRENT}_VOLTAGE-{CC_VOLTAGE}_TIME-{MEASURING_TIME}_INTERVAL-{MEASURING_INTERVAL}.png")
         plt.close()
 
         plt.plot(t_array, U_array)
         plt.xlabel('Time (s)')
         plt.ylabel('Voltage (V)')
-        plt.savefig(f"data/plots/VOLTAGE_PowerSupplyData_CURRENT-{CC_CURRENT}_VOLTAGE-{CC_VOLTAGE}_TIME-{MEASURING_TIME}_INTERVAL-{MEASURING_INTERVAL}.png")
+        plt.savefig(f"data/plots/VOLTAGE_PowerSupplyData_RUN_ID-{RUN_ID}_CURRENT-{CC_CURRENT}_VOLTAGE-{CC_VOLTAGE}_TIME-{MEASURING_TIME}_INTERVAL-{MEASURING_INTERVAL}.png")
         plt.close()
 
-
-        time.sleep(MEASURING_INTERVAL)
-
-    setOnState(ser, False)
-    ser.close()             # close port
-
-    
-    
-
+        setOnState(ser, False)
+        ser.close()             # close port
 
 
 if __name__ == "__main__":
